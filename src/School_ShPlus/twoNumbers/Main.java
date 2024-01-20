266a0c26fc8a577f1d5214ed8ee3050aca6e1186
Andrew Kurylyk <andronniy7@gmail.com>
2024-01-20 16:27:18 +0200

    First

--- src/School_ShPlus/twoNumbers/Main.java
+++ src/School_ShPlus/twoNumbers/Main.java
@@ -2,20 +2,26 @@ package School_ShPlus.twoNumbers;
 
 public class Main {
 
-        public static void main(String[] args) {
-            int result = actionsNumbers(1, 20);
-            System.out.println("Сума: " + result);
-        }
+    public static void main(String[] args) {
 
-        public static int actionsNumbers(int a, int b) {
-            int sum = 0;
-            // Перевіряємо кожне число від a до b
-            for (int i = a; i <= b; i++) {
-                // Перевіряємо, чи число ділиться на 5 або на одне з введених чисел без остачі
-                if (i % 5 == 0 || i % a == 0 || i % b == 0) {
-                    sum += i; // Додаємо число до суми
-                }
-            }
-            return sum;
-        }
+        int result = actionsNumbers(3, 20);
+        System.out.println("Сума: " + result);
     }
+
+    public static int actionsNumbers(int a, int b) {
+        int sum = 0;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start+1; i < end; i++) {
+
+            if (i % 5 == 0 || i % a == 0 || i % b == 0) {
+                sum += i;
+            }
+
+        }
+        return sum;
+    }
+}
+
