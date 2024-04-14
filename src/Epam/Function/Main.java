d5302c6f4e74e74903e3f225a592d6904a26a896
Andrew Kurylyk <andronniy7@gmail.com>
2024-04-14 17:59:57 +0300

    First

--- src/Epam/Function/Main.java
+++ src/Epam/Function/Main.java
@@ -1,10 +1,10 @@
-package Epam.Function;
-
-public class Main {
-    public static void main(String[] args) {
-
-        int result = Result.multiArithmeticElements(5,3,5);
-        System.out.println(result);
-
-    }
-}
+//package Epam.Function;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int result = Result.multiArithmeticElements(5,3,5);
+//        System.out.println(result);
+//
+//    }
+//}
--- src/Epam/Function/Result.java
+++ src/Epam/Function/Result.java
@@ -1,17 +1,17 @@
-package Epam.Function;
-
-public class Result {
-
-    public static int multiArithmeticElements(int a1, int t, int n) {
-        if (n <=0) {
-            throw new IllegalArgumentException("Number is negative!");
-        }
-
-        int multiplication = 1;
-        for (int i = 0; i < n; i++) {
-            multiplication *= a1 + i * t;
-        }
-
-        return multiplication;
-    }
-}
+//package Epam.Function;
+//
+//public class Result {
+//
+//    public static int multiArithmeticElements(int a1, int t, int n) {
+//        if (n <=0) {
+//            throw new IllegalArgumentException("Number is negative!");
+//        }
+//
+//        int multiplication = 1;
+//        for (int i = 0; i < n; i++) {
+//            multiplication *= a1 + i * t;
+//        }
+//
+//        return multiplication;
+//    }
+//}
