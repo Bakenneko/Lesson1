1149854353f08da755b69b1dc176b4eab04d1d7b
Andrew Kurylyk <andronniy7@gmail.com>
2024-06-15 15:44:03 +0300

    First

---
+++ src/Epam/Arrays/task2/IntArrayUtil.java
@@ -0,0 +1,57 @@
+package Epam.Arrays.task2;
+
+public class IntArrayUtil {
+
+    public static int maximumDistance(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int maxValue = Integer.MIN_VALUE;
+        int firstIndex = -1;
+        int lastIndex = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                firstIndex = i;
+                lastIndex = i;
+            } else if (array[i] == maxValue) {
+                lastIndex = i;
+            }
+        }
+
+        if (firstIndex == lastIndex) {
+            return 0;
+        }
+
+        return lastIndex - firstIndex;
+    }
+
+    public static void main(String[] args) {
+        {
+            int[] array = null;
+            System.out.println("result = " + maximumDistance(array));
+        }
+        {
+            int[] array = new int[] {};
+            System.out.println("result = " + maximumDistance(array));
+        }
+        {
+            int[] array = new int[] { 4, 100, 3, 4 };
+            System.out.println("result = " + maximumDistance(array));
+        }
+        {
+            int[] array = new int[] { 5, 50, 50, 4, 5 };
+            System.out.println("result = " + maximumDistance(array));
+        }
+        {
+            int[] array = new int[] { 5, 350, 350, 4, 350 };
+            System.out.println("result = " + maximumDistance(array));
+        }
+        {
+            int[] array = new int[] { 10, 10, 10, 10, 10 };
+            System.out.println("result = " + maximumDistance(array));
+        }
+    }
+
+}
